8ffa158a4c893b429dd11aff5e3e72250319f62e
jim <jim.otieno@student.moringaschool.com>
2023-10-07 15:53:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+function reverse(word){
+  const wordarray=word.split("")
+  const reversedWordarray=wordarray.reverse()
+  const reversedWord=reversedWordarray.join("")
+  return reversedWord
+}
 function isPalindrome(word) {
   // Write your algorithm here
+const reversedWord=reverse(word)
+  if (word === reversedWord) {
+    return true;
+  }
+  else{return false;
+  } 
+
 }
+
+
+
 
 /* 
   Add your pseudocode here
+reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
+
 */
+
 
 /*
   Add written explanation of your solution here
@@ -20,6 +44,22 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
+
